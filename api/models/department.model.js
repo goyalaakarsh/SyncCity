b25b49a63d8d3a7665ea9c7461ddc14b39120d7c
lakkshyag <lakkshyag@gmail.com>
2024-08-21 15:15:49 +0530

    department model change

--- api/models/department.model.js
+++ api/models/department.model.js
@@ -9,6 +9,10 @@ const departmentSchema = new mongoose.Schema({
     adminId: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+    },
 
 }, {timestamps: true});
